b316855eafc10402006cdcfac7e4831e040d4787
ARAFAT <mdarafathossen.py@gmail.com>
2026-02-19 09:33:09 +0600

    Fix case sensitive import path

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import ActivateAccount from "../components/Registration/ActivateAccount";
